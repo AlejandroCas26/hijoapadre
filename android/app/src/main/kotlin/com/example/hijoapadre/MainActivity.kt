a7e30d8dbb97f53adeffac3798cb1ec95639b35a
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-09 21:51:58 -0600

    Creando el HomePage

---
+++ android/app/src/main/kotlin/com/example/hijoapadre/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hijoapadre
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Returning Data Demo'),
+      ),
+      // Create the SelectionButton widget in the next step.
+      body: const Center(
+        child: SelectionButton(),
+      ),
+    );
+  }
+}
