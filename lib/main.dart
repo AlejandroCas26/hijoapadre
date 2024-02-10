ff528277be082a40724175bfda2daa757b6b984b
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-09 21:59:30 -0600

    Agregando los botones para la seleccion

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Returning Data',
+      home: HomeScreen(),
+    ),
+  );
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -7,10 +18,43 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Returning Data Demo'),
       ),
-      // Create the SelectionButton widget in the next step.
       body: const Center(
         child: SelectionButton(),
       ),
     );
   }
 }
+
+class SelectionButton extends StatefulWidget {
+  const SelectionButton({super.key});
+
+  @override
+  State<SelectionButton> createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: const Text('Pick an option, any option!'),
+    );
+  }
+
+  Future<void> _navigateAndDisplaySelection(BuildContext context) async {
+    
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SelectionScreen()),
+    );
+
+    if (!mounted) return;
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('$result')));
+  }
+}
+
